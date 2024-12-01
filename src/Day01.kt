0862d9d6da279e5a4bc4ec62edc0e31736750305
manuel.arlin <manuel.arlin@manomano.com>
2024-12-01 06:56:03 +0100

    day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,25 @@
+import kotlin.math.abs
+
 fun main() {
+    fun getTwoLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val elements = input.map { it.split("[\\s]+".toRegex()) }
+            .map { Pair(it.get(0).toInt(), it.get(1).toInt()) }
+
+        val orderedFirstList = elements.map { it.first }.sorted()
+        val orderedSecondList = elements.map { it.second }.sorted()
+        return Pair(orderedFirstList, orderedSecondList)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (orderedFirstList, orderedSecondList) = getTwoLists(input)
+
+        val diffList = mutableListOf<Int>()
+
+        for (i in 0..<orderedSecondList.size) {
+            diffList.add(abs(orderedSecondList.get(i) - orderedFirstList.get(i)))
+        }
+
+        return diffList.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +27,12 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("2   5")) == 3)
+    check(part1(listOf("5   2")) == 3)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
