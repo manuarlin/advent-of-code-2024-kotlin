6def1db82f79f6eadc054e4b1a9b71174892bafe
manuel.arlin <manuel.arlin@manomano.com>
2024-12-02 08:16:48 +0100

    day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -6,11 +6,28 @@ fun main() {
         var isIncreasing = report[1] - report[0] > 0
 
         for (i in 0..report.size - 2) {
-            var newDiff = report[i + 1] - report[i]
-            if (newDiff > 0 && !isIncreasing || newDiff < 0 && isIncreasing) return false
-            if (abs(newDiff) < 1 || abs(newDiff) > 3) return false
+            var levelDiff = report[i + 1] - report[i]
+            if (
+                (levelDiff > 0 && !isIncreasing || levelDiff < 0 && isIncreasing)
+                || (abs(levelDiff) < 1 || abs(levelDiff) > 3)
+            ) return false
         }
         return true
+    }
+
+    fun checkIfReportIsSafeWithDampener(report: List<Int>): Boolean {
+        if (checkIfReportIsSafe(report)) {
+            return true
+        }
+
+        for (i in report.indices) {
+            val reportWithoutLevel = report.toMutableList()
+            reportWithoutLevel.removeAt(i)
+            if (checkIfReportIsSafe(reportWithoutLevel)) {
+                return true
+            }
+        }
+        return false
     }
 
     fun part1(input: List<String>): Int {
@@ -20,20 +37,21 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-
+        val reports = input.map { it.extractNumbers("\\s".toRegex()) }
+        return reports.count { checkIfReportIsSafeWithDampener(it) }
     }
 
     // Test if implementation meets criteria from the description, like:
     check(part1(listOf("7 6 4 2 1")) == 1)
+    check(part2(listOf("8 6 4 4 1")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 2)
-//    check(part2(testInput) == 31)
+    check(part2(testInput) == 4)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
