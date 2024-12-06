7540844807569c10c35eb957589a342ef628bda5
manuel.arlin <manuel.arlin@manomano.com>
2024-12-06 09:08:38 +0100

    day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -20,12 +20,7 @@ fun main() {
             && currentPosition.first <= maxX && currentPosition.second <= maxY
         ) {
             allPositions.add(currentPosition)
-            val nextPotentialPosition = when (currentMovingWay) {
-                MovingWay.UP -> Pair(currentPosition.first - 1, currentPosition.second)
-                MovingWay.RIGHT -> Pair(currentPosition.first, currentPosition.second + 1)
-                MovingWay.DOWN -> Pair(currentPosition.first + 1, currentPosition.second)
-                MovingWay.LEFT -> Pair(currentPosition.first, currentPosition.second - 1)
-            }
+            val nextPotentialPosition = currentPosition.next(currentMovingWay)
             val symbol = map.getOrDefault(nextPotentialPosition, null)
 
             if (symbol != null && symbol == '#') {
@@ -37,6 +32,31 @@ fun main() {
         return allPositions
     }
 
+    fun willGardBeStuck(map: PuzzleMap, maxX: Int, maxY: Int, newObstacle: Position): Boolean {
+        val allPositionsWithMovingWay = mutableListOf<PositionWithMovingWay>()
+
+        var currentMovingWay = MovingWay.UP
+        var currentPosition = map.filterValues { it == '^' }.keys.first()
+
+        while (currentPosition.first >= 0 && currentPosition.second >= 0
+            && currentPosition.first <= maxX && currentPosition.second <= maxY
+        ) {
+            if (allPositionsWithMovingWay.contains(Pair(currentPosition, currentMovingWay))) {
+                return true
+            }
+            allPositionsWithMovingWay.add(Pair(currentPosition, currentMovingWay))
+            val nextPotentialPosition = currentPosition.next(currentMovingWay)
+            val symbol = map.getOrDefault(nextPotentialPosition, null)
+
+            if ((symbol != null && symbol == '#') || nextPotentialPosition == newObstacle) {
+                currentMovingWay = currentMovingWay.turnOnRight()
+            } else {
+                currentPosition = nextPotentialPosition
+            }
+        }
+        return false
+    }
+
     fun part1(input: List<String>): Int {
         val map = getMap(input)
         val maxX = input.size - 1
@@ -45,18 +65,32 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val map = getMap(input)
+        val maxX = input.size - 1
+        val maxY = input[0].length - 1
+        val possibleNewObstaclePositions = map.filterValues { it == '.' }.keys
+        val positionsForInfiniteLoop = mutableListOf<Position>()
+
+        for (position in possibleNewObstaclePositions) {
+            if (willGardBeStuck(map, maxX, maxY, position)) {
+                positionsForInfiniteLoop.add(position)
+            }
+        }
+
+        return positionsForInfiniteLoop.count()
     }
 
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 41)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day06")
     part1(input).println()
-//    part2(input).println()
+    //TODO Improve running time ?
+    part2(input).println()
 }
 
 enum class MovingWay {
@@ -70,6 +104,14 @@ enum class MovingWay {
     }
 }
 
+fun Position.next(movingWay: MovingWay): Position = when (movingWay) {
+    MovingWay.UP -> Pair(this.first - 1, this.second)
+    MovingWay.RIGHT -> Pair(this.first, this.second + 1)
+    MovingWay.DOWN -> Pair(this.first + 1, this.second)
+    MovingWay.LEFT -> Pair(this.first, this.second - 1)
+}
+
 typealias Position = Pair<Int, Int>
+typealias PositionWithMovingWay = Pair<Position, MovingWay>
 typealias PuzzleMap = MutableMap<Position, Char>
 typealias Positions = MutableList<Position>
